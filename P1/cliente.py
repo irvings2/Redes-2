6c7126d13344438565c73e7cd981e70d9d51a2d1
irvings2 <88854552+irvings2@users.noreply.github.com>
2023-10-06 15:13:18 -0600

    Pruebas

---
+++ P1/cliente.py
@@ -0,0 +1,21 @@
+import socket
+from cryptography.fernet import Fernet
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'SEND archivo.txt')
+    data = s.recv(1024)
+    if data == b'OK':
+        file_data = s.recv(4096)
+        print('Ingresa la llave: ', end=" ")
+        key = input()
+        cipher_suite = Fernet(key.encode())
+        # Desencripta el mensaje si la llave es correcta
+        plain_text = cipher_suite.decrypt(file_data)
+        with open('archivo_descargado.txt', 'wb') as f:
+            f.write(plain_text)
+    else:
+        print('Error al recibir el archivo')
---
+++ P1/servidor.py
@@ -0,0 +1,30 @@
+import socket
+from cryptography.fernet import Fernet
+
+# Genera una clave de cifrado aleatoria
+key = Fernet.generate_key()
+cipher_suite = Fernet(key)
+print(key.decode())
+
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen()
+    print('Servidor escuchando...')
+    conn, addr = s.accept()
+    with conn:
+        print('Conectado por', addr)
+        data = conn.recv(1024)
+        comando, nombre_archivo = data.decode('utf-8').split(' ')
+        if comando == 'SEND':
+            try:
+                with open(nombre_archivo, 'rb') as f:
+                    file_data = f.read()
+                conn.sendall(b'OK')
+                # Encripta el mensaje
+                cipher_text = cipher_suite.encrypt(file_data)
+                conn.sendall(cipher_text)
+            except:
+                conn.sendall(b'ERROR')
